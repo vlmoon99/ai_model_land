d06bb8aa6a1d3db6f6f385d88e317ebde34ad435
unknown <vballiev@gmail.com>
2024-05-29 19:09:07 +0300

    add: core repository(not complete), network(not complete), provider_ai_service(not complete)

---
+++ lib/modules/core/services/provider_ai_service.dart
@@ -0,0 +1,16 @@
+import 'package:ai_model_land/modules/core/models/task_request_model.dart';
+import 'package:ai_model_land/modules/core/models/task_response_model.dart';
+
+abstract class ProviderAiService {
+  Future<TaskResponseModel> runTaskOnTheModel(TaskRequestModel request);
+
+  void checkPlatformGPUAcceleratorPossibilities(String params);
+
+  Future deleteModal();
+
+  Future stopModal();
+
+  Future restartModal();
+
+  Future addModal();
+}
