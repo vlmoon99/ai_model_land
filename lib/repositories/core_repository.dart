525bf61069078f9c9de27b1b9aad2ab9ce5b0142
unknown <vballiev@gmail.com>
2024-06-05 12:03:10 +0300

    update: implements repositories

--- lib/repositories/core_repository.dart
+++ lib/repositories/core_repository.dart
@@ -21,4 +21,44 @@ class CoreRepository {
       networkStorage: NetworkStorage.defaultInstance(),
     );
   }
+
+  Future<void> save({required BaseModel item}) async {
+    await modalRepositories[item.sourceType]?.save(item);
+  }
+
+  Future<void> saveAll(
+      {required List<BaseModel> items,
+      required ModelSourceType sourceType}) async {
+    await modalRepositories[sourceType]?.saveAll(items);
+  }
+
+  Future<BaseModel> read(
+      {required String id, required ModelSourceType sourceType}) async {
+    final modal = await modalRepositories[sourceType]?.read(id);
+    if (modal == null) {
+      throw Exception("This modal not exist");
+    }
+    return modal;
+  }
+
+  Future<List<BaseModel>> readAll({required ModelSourceType sourceType}) async {
+    final modals = await modalRepositories[sourceType]?.readAll();
+    if (modals == null) {
+      throw Exception("This modal not exist");
+    }
+    return modals;
+  }
+
+  Future<void> update({required String key, required BaseModel item}) async {
+    await modalRepositories[item.sourceType]?.update(key, item);
+  }
+
+  Future<void> delete(
+      {required String id, required ModelSourceType sourceType}) async {
+    await modalRepositories[sourceType]?.delete(id);
+  }
+
+  Future<void> deleteAll({required ModelSourceType sourceType}) async {
+    await modalRepositories[sourceType]?.deleteAll();
+  }
 }
