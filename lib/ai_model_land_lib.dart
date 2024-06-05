5b77e54fd19dcc50a60f81ba24a2d45eaf273676
unknown <vballiev@gmail.com>
2024-06-05 14:15:50 +0300

    add: complete repo

--- lib/ai_model_land_lib.dart
+++ lib/ai_model_land_lib.dart
@@ -1,4 +1,5 @@
 import 'package:ai_model_land/modules/core/models/base_model.dart';
+import 'package:ai_model_land/repositories/core_repository.dart';
 import 'package:ai_model_land/repositories/i_repository.dart';
 import 'package:collection/collection.dart';
 import 'package:rxdart/rxdart.dart';
@@ -7,29 +8,40 @@ class AiModelLandLib {
   final BehaviorSubject<List<BaseModel>> modelStream =
       BehaviorSubject<List<BaseModel>>();
 
-  final Repository<BaseModel> baseModelRepository;
+  // final Repository<BaseModel> baseModelRepository;
 
-  AiModelLandLib(this.baseModelRepository) {
-    initAILib();
+  final CoreRepository coreRepository;
+
+  AiModelLandLib(this.coreRepository) {
+    // initAILib();
   }
 
   //factory
-
-  Future<bool> initAILib() async {
-    await baseModelRepository.readAll().then((baseModels) {
-      modelStream.add(baseModels);
-    });
-    return true;
+  factory AiModelLandLib.defaultInstance() {
+    return AiModelLandLib(
+      CoreRepository.defaultInstance(),
+    );
   }
+  // Future<bool> initAILib() async {
+  //   await coreRepository.readAll().then((baseModels) {
+  //     modelStream.add(baseModels);
+  //   });
+  //   return true;
+  // }
 
   Future addModel({required BaseModel baseModel}) async {
-    final isAlreadyExist = (await baseModelRepository.readAll())
-            .firstWhereOrNull((element) => element.id == baseModel.id) !=
-        null;
+    final isAlreadyExist =
+        (await coreRepository.readAll(sourceType: baseModel.sourceType))
+                .firstWhereOrNull((element) => element.id == baseModel.id) !=
+            null;
     if (isAlreadyExist) {
       throw Exception("This model is already exist");
     }
-    final id = ((await baseModelRepository.readAll()).lastOrNull?.id ?? -1) + 1;
+    final id = ((await coreRepository.readAll(sourceType: baseModel.sourceType))
+                .lastOrNull
+                ?.id ??
+            -1) +
+        1;
     final baseModeld = BaseModel(
         id: id,
         source: baseModel.source,
@@ -38,7 +50,24 @@ class AiModelLandLib {
 
     modelStream.value.add(baseModeld);
     modelStream.add(modelStream.value);
-    baseModelRepository.saveAll(modelStream.value);
+    coreRepository.saveAll(
+        items: modelStream.value, sourceType: baseModel.sourceType);
     return baseModeld;
+  }
+
+  Future<bool> deleteAllModelsForType(
+      {required ModelSourceType sourceType}) async {
+    coreRepository.deleteAllModelsForType(sourceType: sourceType);
+    return true;
+  }
+
+  Future<bool> deleteAllModels() async {
+    coreRepository.deleteAllModels();
+    return true;
+  }
+
+  Future<List<BaseModel>> readAllForType(
+      {required ModelSourceType sourceType}) async {
+    return await coreRepository.readAll(sourceType: sourceType);
   }
 }
